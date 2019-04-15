23859f313293a86eaa094b4e57d3c47c6a50c5c3
jurusteve <jstevemu17@gmail.com>
2019-04-15 19:01:16 -0400

    added a sign up form component and stored auth token into localStorage

--- gigapet/src/App.js
+++ gigapet/src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Signup from './containers/Signup/Signup'
+import Home from './containers/MainPage/Home'
 
 class App extends Component {
-  render() {
+  constructor(props){
+    super(props)
+    this.state = {
+      isAuth: false
+    }
+  }
+
+  authenticateUser = ()=>{
+    this.setState({isAuth: !this.state.isAuth})
+  }
+
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+      {this.state.isAuth ? 
+        <Home />
+        :
+        <Signup auth={this.authenticateUser} />
+    }
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ gigapet/src/containers/Signup/Signup.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import { Button, FormGroup, FormControl, FormLabel } from 'react-bootstrap'
+import './Signup.css'
+
+export default class Signup extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            name: "",
+            username: "",
+            email: '',
+            password: ''
+        }
+    }
+    handleSubmit = (e)=>{
+      e.preventDefault()
+      axios.post('https://lambda-gigapet.herokuapp.com/api/auth/register', this.state)
+        .then(res=>{
+          console.log(res.data)
+          if(res.data.token){
+            this.props.auth()
+            localStorage.setItem('GPtoken', res.data.token )
+          }
+        })
+        .catch(e=>{
+          console.log('Error While Signing Up', e)
+        })
+
+    }
+    handleChange = (e)=>{
+      this.setState({[e.target.id]: e.target.value})
+    }
+    validateForm = ()=>{
+      const {password, email} = this.state
+      return password.length > 0 && email.length > 0
+    }
+  render () {
+    const {name, email, username, password} = this.state
+    return (
+      <div className='sign-up'>
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId='name' bissize='large'>
+            <FormLabel>Name</FormLabel>
+            <FormControl
+              autoFocus
+              type='name'
+              value={name}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId='username' bissize='large'>
+            <FormLabel>Username</FormLabel>
+            <FormControl
+              autoFocus
+              type='username'
+              value={username}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId='email' bissize='large'>
+            <FormLabel>Email</FormLabel>
+            <FormControl
+              autoFocus
+              type='email'
+              value={email}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId='password' bissize='large'>
+            <FormLabel>Password</FormLabel>
+            <FormControl
+              value={password}
+              onChange={this.handleChange}
+              type='password'
+            />
+          </FormGroup>
+          <Button
+            block
+            bissize='large'
+            disabled={!this.validateForm()}
+            type='submit'
+          >
+            Sign Up
+          </Button>
+        </form>
+      </div>
+    )
+  }
+}
